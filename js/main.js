2fe6aeed0acd71e3607e089d16e0f1c0ff33f757
Gustavo Pinho <gpinho@voiza.com.br>
2017-10-26 17:25:07 -0200

    ajustes layout

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ jQuery(function ($) {
 
 	$('.navbar-collapse ul li a').on('click', function () {
 		$('html, body').animate({
-			scrollTop: $(this.hash).offset().top - 105
+			scrollTop: $(this.hash).offset().top - 111
 		}, 1000);
 		return false;
 	});
@@ -156,7 +156,7 @@ jQuery(function ($) {
 	smoothScroll.init();
 
 	// portfolio filter
-	$(window).load(function () {
+	$(window).on("load",function () {
 		'use strict';
 		var $portfolio_selectors = $('.portfolio-filter >li>a');
 		var $portfolio = $('.portfolio-items');
@@ -245,23 +245,6 @@ jQuery(function ($) {
 		social_tools: false
 	});
 
-	//Google Map
-	var latitude = $('#google-map').data('latitude');
-	var longitude = $('#google-map').data('longitude');
 
-	function initialize_map() {
-		var myLatlng = new google.maps.LatLng(latitude, longitude);
-		var mapOptions = {
-			zoom: 14,
-			scrollwheel: false,
-			center: myLatlng
-		};
-		var map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
-		var marker = new google.maps.Marker({
-			position: myLatlng,
-			map: map
-		});
-	}
-	google.maps.event.addDomListener(window, 'load', initialize_map);
 
 });
