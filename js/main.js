6f5fa24bb05be11f5df65b48a317ddcff31992d3
Gustavo Pinho <gcdpinho@gmail.com>
2017-10-27 19:28:27 -0200

    ajustes layout

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ jQuery(function ($) {
 
 	$('.navbar-collapse ul li a').on('click', function () {
 		$('html, body').animate({
-			scrollTop: $(this.hash).offset().top - 116.5
+			scrollTop: $(this.hash).offset().top - 117
 		}, 1000);
 		return false;
 	});
